106a70d325a4c47c0432bf9d662b493f2a438304
luismarymo <luchita0420@gmail.com>
2025-04-09 18:42:18 +0200

    Moved the session secret into an environment variable

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app
   .use(bodyParser.json())
   .use(
     session({
-      secret: "secret",
+      secret: process.env.SESSION_SECRET,
       resave: false,
       saveUninitialized: true,
     }),
